09dedcfc6e224c367600a6b6ad64328f6c7efe30
Andrei Dziahel <develop7@develop7.info>
2013-05-20 15:02:06 +0300

    reformatted code

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -20,13 +20,13 @@ var timeout = false;
 function addMarker(id, lat, lon, title) {
   if (markersHash[id] === undefined) { //there's no marker for such user
     loc = new google.maps.LatLng(lat, lon);
-  var marker = new google.maps.Marker({
-    position: loc,
-    map: map,
-    animation: google.maps.Animation.DROP,
-    title: title
-  });
-  markersHash[id] = marker;
+    var marker = new google.maps.Marker({
+      position: loc,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: title
+    });
+    markersHash[id] = marker;
     return marker;
   } else {
     var marker = markersHash[id];
@@ -40,14 +40,14 @@ function addMarker(id, lat, lon, title) {
 }
 
 function clearOverlays() {
-  jQuery.each(markersHash, function(k, v){
+  jQuery.each(markersHash, function (k, v) {
     v.setMap(null)
   });
 }
 
 // Shows any overlays currently in the array
 function showOverlays() {
-  jQuery.each(markersHash, function(k, v){
+  jQuery.each(markersHash, function (k, v) {
     v.setMap(map)
   });
 }
@@ -73,41 +73,41 @@ function searchIt() {
 
 var xpq = null;
 function showResults() {
-  $.getJSON('/users.js?la='+latitude+'&lo='+longitude, function (data) {
+  $.getJSON('/users.js?la=' + latitude + '&lo=' + longitude, function (data) {
     dat = data;
-    $.each(data, function (i,user) {
+    $.each(data, function (i, user) {
       $(function () {
         var marker = addMarker(user['id'], user['latitude'], user['longitude'], user['name']);
-        var info ='<div class="stack"><img src="http://www.gravatar.com/avatar/'+user['gravatar']+'" alt="Gravatar" class="gravatar" />';
-        info += '<h3 class="boxContent">'+user['name']+'</h3></div>';
+        var info = '<div class="stack"><img src="http://www.gravatar.com/avatar/' + user['gravatar'] + '" alt="Gravatar" class="gravatar" />';
+        info += '<h3 class="boxContent">' + user['name'] + '</h3></div>';
         if (isSet(user['address'])) {
-          info += '<div class="boxContent stack">Address: '+user['address']+'</div>';
+          info += '<div class="boxContent stack">Address: ' + user['address'] + '</div>';
         }
         if (isSet(user['phone'])) {
-          info += '<div class="boxContent stack">Phone: <a href="callto:'+user['phone']+'"><b>'+user['phone']+'</b></a></div>';
+          info += '<div class="boxContent stack">Phone: <a href="callto:' + user['phone'] + '"><b>' + user['phone'] + '</b></a></div>';
         }
         info += '<div class="stack">';
         if (isSet(user['aim'])) {
-          info += '<a href="aim:goim?screenname='+user['aim']+'&message=Bitcoin+trade."><img src="/assets/aol_64.png" alt="AOL Instant Messenger" title="AIM User: '+user['aim']+'" class="icon" /></a>';
+          info += '<a href="aim:goim?screenname=' + user['aim'] + '&message=Bitcoin+trade."><img src="/assets/aol_64.png" alt="AOL Instant Messenger" title="AIM User: ' + user['aim'] + '" class="icon" /></a>';
         }
         if (isSet(user['skype'])) {
-          info += '<a href="skype:'+user['skype']+'?call"><img src="/assets/skype_icon_64x64.png" alt="Skype" title="Skype User: '+user['skype']+'" class="icon" /></a>';
+          info += '<a href="skype:' + user['skype'] + '?call"><img src="/assets/skype_icon_64x64.png" alt="Skype" title="Skype User: ' + user['skype'] + '" class="icon" /></a>';
         }
         if (isSet(user['yim'])) {
-          info += '<a href="ymsgr:sendIM?'+user['yim']+'"><img src="/assets/yahoo_64.png" alt="Yahoo Address" title="YIM: '+user['yim']+'" class="icon" /></a>';
+          info += '<a href="ymsgr:sendIM?' + user['yim'] + '"><img src="/assets/yahoo_64.png" alt="Yahoo Address" title="YIM: ' + user['yim'] + '" class="icon" /></a>';
         }
         if (isSet(user['jabber'])) {
-          info += '<a href="xmpp:'+user['jabber']+'?message"><img src="/assets/jabber_icon_64x64.png" title="Jabber: '+user['jabber']+'" alt="Jabber" /></a>';
+          info += '<a href="xmpp:' + user['jabber'] + '?message"><img src="/assets/jabber_icon_64x64.png" title="Jabber: ' + user['jabber'] + '" alt="Jabber" /></a>';
         }
         info += '</div>';
         marker.infotext = info;
-        google.maps.event.addListener(marker, 'click', function(latLng) {
+        google.maps.event.addListener(marker, 'click', function (latLng) {
           xpq = marker;
           infowindow.close();
           infowindow.setContent(marker.infotext);
           infowindow.setPosition(marker.location);
-          infowindow.open(map,marker);
-         });
+          infowindow.open(map, marker);
+        });
       });
     });
   });
@@ -117,12 +117,12 @@ function showResults() {
 function whereami() {
   if ($('#user_latitude').val() == null || $('#user_latitude').val() == '') {
     longitude = geoip_longitude();
-    latitude  = geoip_latitude();
-    city      = geoip_city();
-    country   = geoip_country_name();
+    latitude = geoip_latitude();
+    city = geoip_city();
+    country = geoip_country_name();
   } else {
     longitude = $('#user_longitude').val();
-    latitude  = $('#user_latitude').val();
+    latitude = $('#user_latitude').val();
   }
 }
 
@@ -139,7 +139,7 @@ function initialize() {
     maxWidth: 400,
     content: ''
   });
-  google.maps.event.addListener(map, 'center_changed', function(){
+  google.maps.event.addListener(map, 'center_changed', function () {
     if (timeout !== false) {
       window.clearTimeout(timeout);
     }
@@ -155,24 +155,24 @@ function codeAddress(address, callback) {
   if ($.trim(address) == '') {
     return false;
   }
-  geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        loca = results[0].geometry.location;
-        latitude = loca.lat();
-        longitude = loca.lng();
-        var marker = new google.maps.Marker({
-          map: map,
-          title: "Search Center",
-          icon: 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png',
-          position: results[0].geometry.location
-        });
-        if (callback != null) {
-          callback();
-        }
-      } else {
-        $('#status').html("<h1>Error</h1><p>Geocode was not successful for the following reason: " + status + '</p>');
+  geocoder.geocode({ 'address': address}, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      loca = results[0].geometry.location;
+      latitude = loca.lat();
+      longitude = loca.lng();
+      var marker = new google.maps.Marker({
+        map: map,
+        title: "Search Center",
+        icon: 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png',
+        position: results[0].geometry.location
+      });
+      if (callback != null) {
+        callback();
       }
+    } else {
+      $('#status').html("<h1>Error</h1><p>Geocode was not successful for the following reason: " + status + '</p>');
+    }
   });
 }
 function showUpdate() {
@@ -187,19 +187,19 @@ function success(event, data, status, xhr) {
 }
 function failure(event, xhr, status, error) {
   html = '<h1 style="color: red">Error</h1>';
-  html += '<p>'+xhr.responseText+'</p>';
+  html += '<p>' + xhr.responseText + '</p>';
   $("#status").html(html);
 }
 function gotLocation() {
   // Success, now submit the form
-  $('#user_latitude').val( latitude );
-  $('#user_longitude').val( longitude );
+  $('#user_latitude').val(latitude);
+  $('#user_longitude').val(longitude);
   if (marker != null) {
     marker.setMap(null);
     marker = addMarker(latitude, longitude, "My Location");
   }
   try {
-  $("form.edit_user").submit();
+    $("form.edit_user").submit();
   } catch (e) {
 
   }
@@ -218,11 +218,11 @@ function trySubmit() {
   }
 }
 
-$(document).ready(function(){
-  $('#go').click(function(){
+$(document).ready(function () {
+  $('#go').click(function () {
     searchIt();
   });
-  $('#search').on('keyup', function(e) {
+  $('#search').on('keyup', function (e) {
     var code = (e.keyCode ? e.keyCode : e.which);
     if (code == 13) {
       searchIt();
