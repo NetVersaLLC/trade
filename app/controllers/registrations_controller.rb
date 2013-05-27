6d0ea11b05197e71021a83bddfc8b56bce1d0317
Fiodor Veresciaka <fiodor.veresciaka@gmail.com>
2013-05-27 17:36:10 +0300

    Edit User

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,31 @@ class RegistrationsController < Devise::RegistrationsController
     session[:omniauth] = nil unless @user.new_record?
   end
 
+  def update
+    @user = User.find(current_user.id)
+
+    successfully_updated = if needs_password?(@user, resource_params)
+                             @user.update_with_password(resource_params)
+                           else
+                             resource_params.delete(:current_password)
+                             @user.update_without_password(resource_params)
+                           end
+
+    if successfully_updated
+      set_flash_message :notice, :updated
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def needs_password?(user, params)
+    (user.email != params[:email] && !params[:email].blank?) ||
+        !params[:password].blank?
+  end
 
   def build_resource(*args)
     super
